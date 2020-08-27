1fa8032a5262ba65b416f7a7eac59a2caa893d38
lihy-git <lihy66668@163.com>
2020-08-27 21:52:42 +0800

    aaaa

--- mingrui-shop-parent/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/entity/CategoryEntity.java
+++ mingrui-shop-parent/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/entity/CategoryEntity.java
@@ -1,5 +1,14 @@
 package com.baidu.shop.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 /**
  * @ClassName CategoryEntity
  * @Description: TODO
@@ -7,5 +16,30 @@ package com.baidu.shop.entity;
  * @Date 2020/8/27
  * @Version V1.0
  **/
+@ApiModel(value = "分类是实体类")
+@Data
+@Table(name = "tb_category")
 public class CategoryEntity {
+
+    @Id
+    @ApiModelProperty(value = "主键", example = "1")
+    @NotNull(message = "主键不能为空")
+    private Integer id;
+
+    @ApiModelProperty(value = "主键",example = "1")
+    @NotEmpty(message = "类目名称不能为空")
+    private String name;
+
+    @ApiModelProperty(value = "父级分类",example = "1")
+    @NotEmpty(message = "父级分类不能为空")
+    private Integer parentId;
+
+    @ApiModelProperty(value = "是否是父级节点",example = "1")
+    @NotEmpty(message = "是否是父级节点不能为空")
+    private Integer isParent;
+
+    @ApiModelProperty(value = "排序",example = "1")
+    @NotEmpty(message = "排序指数不能为空")
+    private Integer sort;
+
 }
