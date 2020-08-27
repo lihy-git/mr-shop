ae841efd612b5a1d79ab012a1cace5b6a895ecff
lihy-git <lihy66668@163.com>
2020-08-27 21:49:05 +0800

    Signed-off-by: lihy-git <lihy66668@163.com>

---
+++ mingrui-shop-parent/mingrui-shop-commoms/mingrui-shop-common-core/src/main/java/com/baidu/shop/base/BaseApiService.java
@@ -0,0 +1,11 @@
+package com.baidu.shop.base;
+
+/**
+ * @ClassName BaseApiService
+ * @Description: TODO
+ * @Author lihongyang
+ * @Date 2020/8/27
+ * @Version V1.0
+ **/
+public class BaseApiService {
+}
---
+++ mingrui-shop-parent/mingrui-shop-service-api/mingrui-shop-service-api-xxx/src/main/java/com/baidu/shop/entity/CategoryEntity.java
@@ -0,0 +1,11 @@
+package com.baidu.shop.entity;
+
+/**
+ * @ClassName CategoryEntity
+ * @Description: TODO
+ * @Author lihongyang
+ * @Date 2020/8/27
+ * @Version V1.0
+ **/
+public class CategoryEntity {
+}
