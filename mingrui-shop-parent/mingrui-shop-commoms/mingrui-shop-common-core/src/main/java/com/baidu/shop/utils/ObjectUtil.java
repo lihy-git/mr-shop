a571323c8ec26555c63b8052b80d5f853cc0a935
lihy-git <lihy66668@163.com>
2020-08-28 21:56:33 +0800

    分类管理

---
+++ mingrui-shop-parent/mingrui-shop-commoms/mingrui-shop-common-core/src/main/java/com/baidu/shop/utils/ObjectUtil.java
@@ -0,0 +1,17 @@
+package com.baidu.shop.utils;
+
+/**
+ * @ClassName ObjectUtil
+ * @Description: TODO
+ * @Author lihongyang
+ * @Date 2020/8/28
+ * @Version V1.0
+ **/
+public class ObjectUtil {
+
+    public static Boolean isNull(Object obj){
+
+        return null == obj;
+    }
+
+}
--- mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
+++ mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,15 @@
 package com.baidu.shop.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baidu.shop.base.BaseApiService;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.entity.CategoryEntity;
 import com.baidu.shop.mapper.CategoryMapper;
 import com.baidu.shop.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
@@ -34,5 +37,70 @@ public class CategoryServiceImpl extends BaseApiService implements CategoryServi
 
         return this.setResultSuccess(list);
     }
+
+    @Transactional
+    @Override
+    public Result<JSONObject> saveCategory(CategoryEntity categoryEntity) {
+
+        CategoryEntity parentCateEntity = new CategoryEntity();
+
+        parentCateEntity.setId(categoryEntity.getParentId());
+
+        parentCateEntity.setIsParent(1);
+
+        categoryMapper.updateByPrimaryKeySelective(parentCateEntity);
+
+        categoryMapper.insertSelective(categoryEntity);
+
+        return this.setResultSuccess();
+    }
+
+    @Transactional
+    @Override
+    public Result<JSONObject> editCategory(CategoryEntity categoryEntity) {
+
+        categoryMapper.updateByPrimaryKeySelective(categoryEntity);
+
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<JSONObject> deleteCategory(Integer id) {
+        CategoryEntity categoryEntity = categoryMapper.selectByPrimaryKey(id);
+
+        if (categoryEntity == null) {
+
+            return this.setResultError("当前id不存在");
+
+        }
+
+        if(categoryEntity.getIsParent() == 1){
+
+            return this.setResultError("当前节点为父节点,不能删除");
+
+        }
+
+        Example example = new Example(CategoryEntity.class);
+
+        example.createCriteria().andEqualTo("parentId",categoryEntity.getParentId());
+
+        List<CategoryEntity> list = categoryMapper.selectByExample(example);
+
+        if(list.size() == 1){//将父节点的isParent状态改为0
+
+            CategoryEntity parentCateEntity = new CategoryEntity();
+
+            parentCateEntity.setId(categoryEntity.getParentId());
+
+            parentCateEntity.setIsParent(0);
+
+            categoryMapper.updateByPrimaryKeySelective(parentCateEntity);
+        }
+
+        categoryMapper.deleteByPrimaryKey(id);//执行删除
+
+        return this.setResultSuccess();
+    }
+
 
 }
