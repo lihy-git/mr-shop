212598fffd18c58d6b6ab601554925b2de0320e9
lihy-git <lihy66668@163.com>
2020-10-14 14:13:54 +0800

    注册功能-校验用户名手机号,给手机发送验证码

---
+++ mingrui-shop-parent/mingrui-shop-service/mingrui-shop-service-user/src/main/java/com/baidu/shop/service/impl/UserServiceImpl.java
@@ -0,0 +1,76 @@
+package com.baidu.shop.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.baidu.shop.base.BaseApiService;
+import com.baidu.shop.base.Result;
+import com.baidu.shop.constant.UserConstant;
+import com.baidu.shop.dto.UserDTO;
+import com.baidu.shop.entity.UserEntity;
+import com.baidu.shop.mapper.UserMapper;
+import com.baidu.shop.service.UserService;
+import com.baidu.shop.utils.BCryptUtil;
+import com.baidu.shop.utils.BaiduBeanUtil;
+import com.baidu.shop.utils.LuosimaoDuanxinUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
+
+import javax.annotation.Resource;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @ClassName UserServiceImpl
+ * @Description: TODO
+ * @Author lihongyang
+ * @Date 2020/10/13
+ * @Version V1.0
+ **/
+@Slf4j
+@RestController
+public class UserServiceImpl extends BaseApiService implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public Result<JSONObject> register(UserDTO userDTO) {
+        UserEntity userEntity = BaiduBeanUtil.copyProperties(userDTO, UserEntity.class);
+        userEntity.setPassword(BCryptUtil.hashpw(userEntity.getPassword(),BCryptUtil.gensalt()));
+        userEntity.setCreated(new Date());
+
+        userMapper.insertSelective(userEntity);
+        return this.setResultSuccess();
+    }
+
+    @Override
+    public Result<List<UserEntity>> checkUserNameOrPhone(String value, Integer type) {
+
+        Example example = new Example(UserEntity.class);
+        Example.Criteria criteria = example.createCriteria();
+
+        if(type == UserConstant.USER_TYPE_USERNAME){
+
+            criteria.andEqualTo("username",value);
+        }else if(type == UserConstant.USER_TYPE_PHONE){
+
+            criteria.andEqualTo("phone",value);
+        }
+        List<UserEntity> userEntities = userMapper.selectByExample(example);
+
+        return this.setResultSuccess(userEntities);
+    }
+
+    @Override
+    public Result<JSONObject> sendValidCode(UserDTO userDTO) {
+        //生成随机6位验证码
+        String code = (int)((Math.random() * 9 + 1) * 100000) + "";
+        //发送短信验证码
+        LuosimaoDuanxinUtil.SendCode(userDTO.getPhone(),code);
+
+        //短信条数只有10条,不够我们测试.所以就不发送短信验证码了,直接在控制台打印就可以
+        log.debug("向手机号码:{} 发送验证码:{}",userDTO.getPhone(),code);
+
+        return this.setResultSuccess();
+    }
+}
